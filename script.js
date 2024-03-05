d5ba8e3ac3202413bab7dc0c8fcf85298a5affb2
Prabhashana1 <sumuduprabashana36@gmail.com>
2024-03-05 18:46:57 +0530

    all pages and functions added

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ $(document).ready(function() {
     // Set interval to automatically change slides
     var slideInterval = setInterval(showNextSlide, 3000);
   });
+
+
+  $(document).ready(function() {
+    $('#contactForm').submit(function(e) {
+      e.preventDefault();
+      var name = $('#name').val();
+      var email = $('#email').val();
+      var message = $('#message').val();
+
+      // Check if the fields are not empty
+      if (name.trim() === '' || email.trim() === '' || message.trim() === '') {
+        $('.invalid-feedback').css('display', 'block');
+        return;
+      }
+
+      // Check if the email is valid
+      var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        $('#email').next('.invalid-feedback').css('display', 'block').text('Please enter a valid email address.');
+        return;
+      }
+
+      // If all validations pass, submit the form
+      this.submit();
+    });
+  });
